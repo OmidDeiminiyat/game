1be6e11284cfa7e49891abbbd74d4f7861ab8a78
OmidDeiminiyat <omid.deiminiyat@yahoo.com>
2024-12-19 14:55:36 +0100

    Done

--- scripts/riveAction.js
+++ scripts/riveAction.js
@@ -1,5 +1,16 @@
 import { isNormal } from "./riveControler.js";
 import { isFirst } from "./riveControler.js";
+import { isHealth } from "./riveControler.js";
+import { isEat } from "./riveControler.js";
+import { myBool } from "./riveControler.js";
+import { socket } from "./socket/socketController.js";
+
+
+
+
+
+const test = isHealth;
+console.log(test);
 
 let feedButton = document.getElementById('feedButton');
 
@@ -7,6 +18,7 @@ window.addEventListener("keydown", (ev) => {
 
     console.log(ev.key);
     
+
     if (ev.key === 'ArrowUp') {
         console.log('its up');
         
@@ -24,7 +36,33 @@ window.addEventListener("keydown", (ev) => {
         }
         second();
     }
+
+    if (ev.key === 'a') {
+        const eating = () => {
+            isEat.fire();
+
+            socket.emit('feed', {isEat: 'client feeds'});
+        };
+        eating();
+
+    function updateHealthValue(amount) {
+        
+        if (isHealth.value) {
+            console.log(isHealth.value);
+             isHealth.value += amount; 
+        } else {
+          console.error('value is not available.');
+        }
+      }
+      updateHealthValue(10);
+    }
+
+ 
+      
+    
 })
+
+
 
 const feed = () => {
     isNormal.fire();
--- scripts/riveControler.js
+++ scripts/riveControler.js
@@ -1,19 +1,31 @@
 let isNormal;
 let isFirst;
-const riveInstance = new rive.Rive({
+let isHealth;
+let isEat;
+let myBool;
+
+let riveInstance = new rive.Rive({
 src: "../assets/anim.riv", 
 canvas: document.getElementById('riveCanvas'),
 autoplay: true,
-stateMachines: 'controler',
+stateMachines: ['controler', 'healthControl'], 
 onLoad: () => {
     riveInstance.resizeDrawingSurfaceToCanvas();
     let controler_input = riveInstance.stateMachineInputs('controler')
     console.log('controler', controler_input);
-    
+
+    let healthControl = riveInstance.stateMachineInputs('healthControl')
+    console.log('health', healthControl);
+
      isNormal = controler_input.find((item) => item.name === 'normal');
      isFirst = controler_input.find((item) => item.name === 'timeline1');
+    isEat = controler_input.find((item) => item.name === 'eat');
+    myBool = controler_input.find((item) => item.name === 'Bool');
+
+     isHealth = healthControl.find((item) => item.name === 'numHealth');
+   
 
 },
 });
 
-export {isNormal, isFirst};
+export {isNormal, isFirst, isHealth, isEat, myBool};
